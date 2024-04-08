0ec354ba7ee6c6020c5648ad48e723c7e04e5228
FranBowden <francesca.bowden22@bathspa.ac.uk>
2024-04-08 23:46:35 +1000

    Model gltf added

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 import * as THREE from 'three';
-//import { OrbitControls } from 'three/addons/controls/OrbitControls.js' //lets just move around in scene view
-//import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; //load 3d Models
-
-/*Controls */
-//const controls = new OrbitControls( camera, renderer.domElement );
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 //creating a scene 
 const scene = new THREE.Scene();
@@ -13,17 +10,81 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//loading 3D model
+var controls = new OrbitControls( camera, renderer.domElement );
 
-const loader = new GLTFLoader();
+var cameralight = new THREE.PointLight( new THREE.Color(1,1,1), 0.5 );
+camera.position.set(0,0,20);
+camera.lookAt(0,0,1);
+camera.add( cameralight );
+scene.add(camera);
 
-loader.load( 'ferrari.glb', function ( gltf ) {
+ //MESH LOADING
+ const loader = new GLTFLoader();
+ 
+ // Load a glTF resource
+ loader.load(
+	 // resource URL
+	 '/model/scene.gltf',
+	 // called when the resource is loaded
+	 function ( gltf ) {
+ 
+		 scene.add( gltf.scene );
+ 
+		 gltf.animations; // Array<THREE.AnimationClip>
+		 gltf.scene; // THREE.Group
+		 gltf.scenes; // Array<THREE.Group>
+		 gltf.cameras; // Array<THREE.Camera>
+		 gltf.asset; // Object
+ 
+	 },
+	 // called while loading is progressing
+	 function ( xhr ) {
+ 
+		 console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+ 
+	 },
+	 // called when loading has errors
+	 function ( error ) {
+ 
+		 console.log( 'An error happened' );
+ 
+	 }
+ );
 
-	scene.add( gltf.scene );
+ // instantiate a loader
+const texloader = new THREE.TextureLoader();
 
-}, undefined, function ( error ) {
 
-	console.error( error );
+const texture = Promise.all([texloader.load('texture1.jpg'), texloader.load('texture2.jpg')], (resolve, reject) => {
+    resolve(texture);
+}).then(result => {
+    // result in array of textures
+});
 
-} );
 
+var MyUpdateLoop = function ( )
+{
+  renderer.render(scene,camera);
+  requestAnimationFrame(MyUpdateLoop);
+};
+
+requestAnimationFrame(MyUpdateLoop);
+
+//this function is called when the window is resized
+var MyResize = function ( )
+{
+  //get the new sizes
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  //then update the renderer
+  renderer.setSize(width,height);
+  //and update the aspect ratio of the camera
+  camera.aspect = width/height;
+  //update the projection matrix given the new values
+  camera.updateProjectionMatrix();
+
+  //and finally render the scene again
+  renderer.render(scene,camera);
+};
+//link the resize of the window to the update of the camera
+window.addEventListener( 'resize', MyResize);
